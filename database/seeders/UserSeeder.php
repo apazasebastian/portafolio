7d8f7d1a57cee96b2a28ad610fa5ec32795bafab
sebastianAM <apazasebastian@gmail.com>
2026-01-12 02:48:17 -0300

    usuariosfaltantes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,23 +10,63 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Solo crear si no existen
+        // Jefe de Recintos
         User::firstOrCreate(
-            ['email' => 'jefe.recintos@municipalidadarica.cl'],
+            ['email' => 'silverwithlotus@gmail.com'],
             [
                 'name' => 'Jefe de Recintos',
                 'password' => Hash::make('password'),
                 'role' => 'jefe_recintos',
+                'email_verified_at' => now(),
+                'recinto_asignado_id' => null,
                 'activo' => true,
             ]
         );
 
+        // Encargado Epicentro 1
         User::firstOrCreate(
-            ['email' => 'admin@arica.cl'],
+            ['email' => 'carlosapazac33@gmail.com'],
             [
-                'name' => 'Administrador',
+                'name' => 'Encargado Epicentro 1',
                 'password' => Hash::make('password'),
-                'role' => 'admin',
+                'role' => 'encargado_recinto',
+                'recinto_asignado_id' => 1,
+                'activo' => true,
+            ]
+        );
+
+        // Encargado Epicentro 2
+        User::firstOrCreate(
+            ['email' => 'gomezchurabrayan@gmail.com'],
+            [
+                'name' => 'Encargado Epicentro 2',
+                'password' => Hash::make('password'),
+                'role' => 'encargado_recinto',
+                'recinto_asignado_id' => 2,
+                'activo' => true,
+            ]
+        );
+
+        // Encargado Fortín Sotomayor
+        User::firstOrCreate(
+            ['email' => 'encargado.fortin@municipalidadarica.cl'],
+            [
+                'name' => 'Encargado Fortín Sotomayor',
+                'password' => Hash::make('password'),
+                'role' => 'encargado_recinto',
+                'recinto_asignado_id' => 3,
+                'activo' => true,
+            ]
+        );
+
+        // Encargado Piscina Olímpica
+        User::firstOrCreate(
+            ['email' => 'apazasebastian@gmail.com'],
+            [
+                'name' => 'Encargado Piscina Olímpica',
+                'password' => Hash::make('password'),
+                'role' => 'encargado_recinto',
+                'recinto_asignado_id' => 4,
                 'activo' => true,
             ]
         );
